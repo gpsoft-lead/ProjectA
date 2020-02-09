b4e349f369e4c4d3e40a2d6b1a5ae7c6f3fc4221
Gangaprasad25 <gangaprasad.tv@gmail.com>
2020-02-09 23:53:38 +0530

    crm url updated

--- src/test/java/com/gpsoft/tests/LoginTest.java
+++ src/test/java/com/gpsoft/tests/LoginTest.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -21,8 +22,9 @@ public class LoginTest {
 	}
 	@Test
 	public void titleTest() {
-		driver.navigate().to("https://demo.vtiger.com/vtigercrm/");
+		driver.navigate().to("https://classic.crmpro.com/login.cfm");
 		System.out.println(driver.getTitle());
+		Assert.assertEquals(driver.getTitle(), "CRMPRO LogIn Screen");
 	}
 	@AfterMethod
 	public void tearDown() {
